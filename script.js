32384e9cf5f3a68245311b75142d0ba730ae3f6c
Sala Sergiu <121624777+Sala-Sergiu@users.noreply.github.com>
2023-05-14 12:44:04 +0300

    Making the template

---
+++ script.js
@@ -0,0 +1,5 @@
+// title
+// main section divided in 2
+// first section are the buttons and current color display and  the second is the canvas
+// left section has the display, select color button, clear canvas button and choose how many squares button
+// right side is the canvas, player clicks and holds to paint
