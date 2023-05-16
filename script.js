cf26b2820b0d9490361d7df4e1a2d7814cf9f657
Sala Sergiu <121624777+Sala-Sergiu@users.noreply.github.com>
2023-05-16 22:25:44 +0300

    Finished the project

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-// title
-// main section divided in 2
-// first section are the buttons and current color display and  the second is the canvas
-// left section has the display, select color button, clear canvas button and choose how many squares button
-// right side is the canvas, player clicks and holds to paint
+let color = "black";
+let click = false;
+
+const colorSquare = function () {
+  if (click) {
+    if (color === "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
+    }
+  }
+};
+
+const changeColor = function (choice) {
+  color = choice;
+};
+
+const populateCanvas = function (size) {
+  let canvas = document.getElementById("drawing__canvas");
+  let squares = canvas.querySelectorAll("div");
+  squares.forEach((square) => square.remove());
+
+  canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
+    canvas.insertAdjacentElement("beforeend", square);
+  }
+};
+
+populateCanvas(16);
+
+const changeSize = function (input) {
+  if (input >= 8 && input <= 64) {
+    populateCanvas(input);
+  } else {
+    console.log("invalid input");
+  }
+};
+
+const resetCanvas = function () {
+  let canvas = document.getElementById("drawing__canvas");
+  let squares = canvas.querySelectorAll("div");
+  squares.forEach((square) => (square.style.backgroundColor = "white"));
+};
+
+document.querySelector("#drawing__canvas").addEventListener("click", () => {
+  click = !click;
+});
